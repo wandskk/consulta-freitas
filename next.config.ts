dc0e4ce82d4db670cc1d315376ec208b30b45ed9
Wanderson Kenedy <wandersonkenedy13@gmail.com>
2026-07-01 10:55:58 -0300

    change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ['192.168.0.163'],
+  allowedDevOrigins: ['192.168.0.163, 192.168.0.175'],
   serverExternalPackages: ['mssql', 'msnodesqlv8'],
 }
 
